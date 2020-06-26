d59bdd1001136e7735d22ef94bf1ddde71d9f58f
mrblacklicorice <61810026+mrblacklicorice@users.noreply.github.com>
2020-06-26 04:17:14 -0500

    Made level generation a 100 times better

--- 2d_terrarin_generation/2d_level_generation.js
+++ 2d_terrarin_generation/2d_level_generation.js
@@ -1,85 +1,177 @@
-var scl = 50;
+var scale = 50;
 var xf = 0;
 var yf = 0;
-var pixel = 10;
-var sealvl = 0.30;
+var pixel;
+var orig_amp = 1;
+var orig_fre = 1;
+var octaves = 8;
+var persistence = 0.5;
+var lacunarity = 2;
+var speed = 10;
+var seed = Math.ceil(Math.random() * 65536);
+noise.seed(seed);
+document.getElementById('seed_in').value = seed;
+document.getElementById('seed_out').innerHTML = seed;
+var arr = [];
+var colors = ['#0f5e9c', '#2389da', '#1ca3ec', '#d2b98b', '#88ab55', '#569944', '#337755', '#6e6a61', '#625a56', '#c3c4ba', '#dddde3'];
+var values = [0.29, 0.33, 0.35, 0.38, 0.47, 0.55, 0.64, 0.77, 0.88, 0.98, 1.00];
+
+for (let i = 0; i < values.length; i++) {
+    document.getElementById(i + 'c').value = colors[i];
+    document.getElementById(i + 'n').value = values[i];
+}
 
 var canvas = document.getElementsByTagName('canvas')[0];
-canvas.width = (Math.floor(window.innerWidth / pixel)) * pixel;
-canvas.height = (Math.floor(window.innerHeight / pixel)) * pixel;
-canvas.style.position = 'absolute';
-canvas.style.padding = '0px';
-canvas.style.left = (window.innerWidth - canvas.width) / 2 + 'px';
-canvas.style.top = (window.innerHeight - canvas.height) / 2 + 'px';
 var context = canvas.getContext('2d');
-noise.seed(Math.random());
-var colorarray;
+canvas.width = window.innerHeight - 20;
+canvas.height = window.innerHeight - 20;
+pixel = canvas.height / 128;
+canvas.style.position = 'absolute';
+canvas.style.padding = '10px';
+canvas.style.left = '0px';
+canvas.style.top = '0px';
+
+document.getElementsByClassName('menu')[0].style.width = (window.innerWidth - window.innerHeight) + 'px';
 
 main();
 
+document.getElementById('generate').addEventListener('click', function () {
+    this.setAttribute("disabled", "true");
+    scale = parseFloat(document.getElementById('scale_in').value);
+    xf = Math.round(parseFloat(document.getElementById('xf').value));
+    yf = Math.round(parseFloat(document.getElementById('yf').value));
+    pixel = parseFloat(canvas.height / document.getElementById('pixel_in').value);
+    orig_amp = parseFloat(document.getElementById('amplitude_in').value);
+    orig_fre = parseFloat(document.getElementById('frequency_in').value);
+    octaves = parseFloat(document.getElementById('octaves_in').value);
+    persistence = parseFloat(document.getElementById('persistence_in').value) / 10;
+    lacunarity = parseFloat(document.getElementById('lacunarity_in').value);
+    speed = parseFloat(document.getElementById('speed_in').value);
+    seed = parseFloat(document.getElementById('seed_in').value);
+    for (let i = 0; i < values.length; i++) {
+        colors[i] = document.getElementById(i + 'c').value;
+        values[i] = document.getElementById(i + 'n').value;
+    }
+    noise.seed(seed);
+    main();
+});
+
 document.addEventListener('keydown', function (event) {
     if (event.key == "ArrowUp") {
-        yf -= 10;
+        yf -= speed;
         main();
+        document.getElementById('xf').value = xf;
+        document.getElementById('yf').value = yf;
     } else if (event.key == "ArrowDown") {
-        yf += 10;
+        yf += speed;
         main();
+        document.getElementById('xf').value = xf;
+        document.getElementById('yf').value = yf;
     } else if (event.key == "ArrowRight") {
-        xf += 10;
+        xf += speed;
         main();
+        document.getElementById('xf').value = xf;
+        document.getElementById('yf').value = yf;
     } else if (event.key == "ArrowLeft") {
-        xf -= 10;
+        xf -= speed;
         main();
+        document.getElementById('xf').value = xf;
+        document.getElementById('yf').value = yf;
     }
 });
 
 function main() {
-    colorarray = new Array(canvas.width / pixel);
+    var colorarray = new Array(Math.round(canvas.width / pixel));
     for (let i = 0; i < colorarray.length; i++) {
-        colorarray[i] = new Array(canvas.height / pixel);
+        colorarray[i] = new Array(Math.round(canvas.height / pixel));
     }
+
+    arr = [];
 
     for (var x = 0; x < canvas.width / pixel; x++) {
         for (var y = 0; y < canvas.height / pixel; y++) {
-            colorarray[x][y] = map(noise.perlin2((x + xf) / scl, (y + yf) / scl), -1, 1, 0, 1);
-
-            if (colorarray[x][y] < sealvl - 0.06) {
-                context.fillStyle = '#0f5e9c';
-            } else if (colorarray[x][y] < sealvl - 0.02) {
-                context.fillStyle = '#2389da';
-            } else if (colorarray[x][y] < sealvl) {
-                context.fillStyle = '#1ca3ec';
-            } else if (colorarray[x][y] < sealvl + 0.03) {
-                context.fillStyle = '#d2b98b';
-            } else if (colorarray[x][y] < sealvl + 0.12) {
-                context.fillStyle = '#88ab55';
-            } else if (colorarray[x][y] < sealvl + 0.2) {
-                context.fillStyle = '#569944';
-            } else if (colorarray[x][y] < sealvl + 0.29) {
-                context.fillStyle = '#438855';
-            } else if (colorarray[x][y] < sealvl + 0.42) {
-                context.fillStyle = '#337755';
-            } else if (colorarray[x][y] < sealvl + 0.53) {
-                context.fillStyle = '#888888';
-            } else if (colorarray[x][y] < sealvl + 0.63) {
-                context.fillStyle = '#bcbcaa';
-            } else if (colorarray[x][y] < sealvl + 0.70) {
-                context.fillStyle = '#dddde3';
+            var amplitude = orig_amp;
+            var frequency = orig_fre;
+            colorarray[x][y] = 0;
+            for (let o = 0; o < octaves; o++) {
+                colorarray[x][y] += noise.perlin2((x + xf) / scale * frequency, (y + yf) / scale * frequency) * amplitude;
+                amplitude *= persistence;
+                frequency *= lacunarity;
             }
+            arr.push(colorarray[x][y]);
+        }
+    }
+
+    var max = arr.reduce(function (a, b) {
+        return Math.max(a, b);
+    });
+
+    var min = arr.reduce(function (a, b) {
+        return Math.min(a, b);
+    });
+
+    for (var x = 0; x < canvas.width / pixel; x++) {
+        for (var y = 0; y < canvas.height / pixel; y++) {
+            colorarray[x][y] = map(colorarray[x][y], min, max, 0, 1);
+            context.fillStyle = color(colorarray[x][y]);
             context.fillRect(x * pixel, y * pixel, pixel, pixel);
         }
     }
-    context.fillStyle = 'White';
-    context.font = "50px Arial";
-    context.fillText("Use Arrow Keys to Move", 0, 40);
+    document.getElementById('generate').removeAttribute("disabled");
 }
-
 
 function map(input, rmin, rmax, min, max) {
     return (input - rmin) * (max - min) / (rmax - rmin) + min;
 }
 
-// } else {
+function color(v) {
+    var col = colors[0];
+    for (let i = 0; i < values.length; i++) {
+        if (v > values[i]) {
+            col = colors[i];
+        } else {
+            return col;
+        }
+    }
+    return col;
+}
+
 //     colorarray[x][y] *= 255;
 //     colorarray[x][y] = Math.ceil(colorarray[x][y]);
 //     context.fillStyle = '#' + colorarray[x][y].toString(16) + colorarray[x][y].toString(16) + colorarray[x][y].toString(16);
+
+document.getElementById('scale_in').onchange = function () {
+    document.getElementById('scale_out').innerHTML = this.value;
+}
+
+document.getElementById('pixel_in').onchange = function () {
+    document.getElementById('pixel_out').innerHTML = this.value;
+}
+
+document.getElementById('amplitude_in').onchange = function () {
+    document.getElementById('amplitude_out').innerHTML = this.value;
+}
+
+document.getElementById('frequency_in').onchange = function () {
+    document.getElementById('frequency_out').innerHTML = this.value;
+}
+
+document.getElementById('persistence_in').onchange = function () {
+    document.getElementById('persistence_out').innerHTML = this.value / 10;
+}
+
+document.getElementById('lacunarity_in').onchange = function () {
+    document.getElementById('lacunarity_out').innerHTML = this.value;
+}
+
+document.getElementById('speed_in').onchange = function () {
+    document.getElementById('speed_out').innerHTML = this.value;
+}
+
+document.getElementById('seed_in').onchange = function () {
+    document.getElementById('seed_out').innerHTML = this.value;
+}
+
+document.getElementById('octaves_in').onchange = function () {
+    document.getElementById('octaves_out').innerHTML = this.value;
+}
