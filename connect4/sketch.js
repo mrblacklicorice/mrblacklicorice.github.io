e8ccd704c53b4f560d599d90b0d83d39e0ff2cd9
mrblacklicorice <61810026+mrblacklicorice@users.noreply.github.com>
2022-03-17 23:19:19 -0500

    thing

--- connect4/sketch.js
+++ connect4/sketch.js
@@ -4,9 +4,9 @@ let side_bar = pixel;
 
 let offset = pixel / 5;
 
-const cols = 8;
+// const cols = 7;
 
-const rows = 8;
+// const rows = 6;
 
 function setup() {
 	canvas = createCanvas(pixel * cols + (offset * 2) + (side_bar * offset * 2), pixel * rows + (offset * 2) + (offset * 10));
@@ -24,3 +24,42 @@ function setup() {
 function draw() {
 	clear();
 }
+
+
+const cols = 7;
+
+const rows = 6;
+
+// x and o
+var d = [
+	["-", "-", "-", "-", "-", "-", "-"],
+	["-", "-", "-", "-", "-", "-", "-"],
+	["-", "x", "-", "-", "-", "-", "-"],
+	["-", "-", "-", "-", "-", "-", "-"],
+	["-", "-", "-", "-", "-", "-", "-"],
+	["-", "-", "-", "-", "-", "-", "-"],
+];
+
+function check_win(p) {
+	var tr, tc, i;
+	var sr = [1, -1, 0, 0, 1, -1];
+	var sc = [0, 0, 1, -1, 1, -1];
+	for (let r = 0; r < d.length; r++) {
+		for (let c = 0; c < d[r].length; c++) {
+			if (d[r][c] == p) {
+				tr = 0, tc = 0, i = 1;
+				while (isInBorder(r + tr, c + tc) && d[r + tr][c + tc] == p) {
+					if (i == 4) return true;
+					tr++;
+					i++;
+				}
+			}
+		}
+	}
+}
+
+function isInBorder(r, c) {
+	return (r > -1 && r < rows && c > -1 && c < cols);
+}
+
+// check_win("x");
