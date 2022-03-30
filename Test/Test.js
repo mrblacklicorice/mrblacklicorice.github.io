cb9011619c00ac6d1a020daf760b17159a0891ca
mrblacklicorice <61810026+mrblacklicorice@users.noreply.github.com>
2022-03-29 23:20:02 -0500

    yarr

--- Test/Test.js
+++ Test/Test.js
@@ -818,7 +818,40 @@ var minEatingSpeed = function (piles, h) {
   return k;
 };
 
-// console.log(reverseWords("Hello Guys"));
-// console.log(longestCommonPrefix(["asadasb", "asadb"]));
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function (nums) {
+  var t = new Array(nums.length - 1).fill(false);
+  for (let i = 0; i < nums.length; i++) {
+    if (t[nums[i]]) return nums[i];
+    else t[nums[i]] = !t[nums[i]];
+  }
+};
 
-// function calculateDaysBetweenDates(begin, end) {
+// console.log(findDuplicate([1, 3, 4, 2, 2]));
+// // 2
+
+let searchMatrix = function (matrix, target) {
+
+  var i = matrix.length - 1;
+  while (matrix[i][0] > target) {
+    i--;
+    if (i == -1) return false;
+  }
+
+  var start = 0, end = matrix[i].length - 1;
+
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (matrix[i][mid] === target) return true;
+    else if (matrix[i][mid] < target) start = mid + 1;
+    else end = mid - 1;
+  }
+
+  return false;
+}
+
+// console.log(searchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 3));
--- connect4/sketch.js
+++ connect4/sketch.js
@@ -31,29 +31,35 @@ const cols = 7;
 const rows = 6;
 
 // x and o
-var d = [
-	["-", "-", "-", "-", "-", "-", "-"],
-	["-", "-", "-", "-", "-", "-", "-"],
-	["-", "x", "x", "x", "x", "-", "-"],
-	["-", "-", "-", "-", "-", "-", "-"],
-	["-", "-", "-", "-", "-", "-", "-"],
-	["-", "-", "-", "-", "-", "-", "-"],
-];
+var d =
+	[
+		["-", "-", "-", "-", "-", "-", "-"],
+		["-", "-", "-", "x", "-", "-", "-"],
+		["-", "-", "x", "o", "-", "-", "-"],
+		["o", "o", "-", "o", "-", "-", "-"],
+		["x", "o", "x", "x", "-", "-", "-"],
+		["x", "o", "o", "o", "-", "-", "-"],
+	];
+
+
 
 function check_win(p) {
 	var tr, tc, i;
 	for (let r = 0; r < d.length; r++) {
 		for (let c = 0; c < d[r].length; c++) {
 			if (d[r][c] == p) {
-				for (let l = -1; l < 1; l++) {
-					for (let k = -1; k < 1; k++) {
-						tr = 0, tc = 0, i = 1;
+				for (let l = -1; l <= 1; l++) {
+					for (let k = -1; k <= 1; k++) {
 						if (!(l == 0 && k == 0)) {
+							tr = l;
+							tc = k;
+							i = 1;
 							while (isInBorder(r + tr, c + tc) && d[r + tr][c + tc] == p) {
-								if (i == 4) return true;
+								if (r == 4 && c == 0) console.log(`(${r + tr + 1}, ${c + tc + 1}, ${i})`);
 								tr += l;
 								tc += k;
 								i++;
+								if (i == 4) return true;
 							}
 						}
 					}
@@ -68,4 +74,4 @@ function isInBorder(r, c) {
 	return (r > -1 && r < rows && c > -1 && c < cols);
 }
 
-console.log(check_win("x"));
+console.log(check_win("o"));
