c557b710925e46ed057a8dab1a20d5879418b8fc
Girish Dodda <girishbdodda@gmail.com>
2022-12-29 15:53:03 -0600

    thing

--- battleship-generator/app.js
+++ battleship-generator/app.js
@@ -4,10 +4,10 @@ var join_game = document.getElementById("join_game");
 var new_game = document.getElementById("new_game");
 
 new_game.addEventListener("click", () => {
-    window.location.href = "https://mrblacklicorice.github.io/battleship/";
+    window.location.href = "https://battleship-53140.web.app/";
 });
 
 join_game.addEventListener("click", () => {
     if (!(gamecode.valueAsNumber > 99999 || gamecode.valueAsNumber < 10000))
-        window.location.href = "https://mrblacklicorice.github.io/battleship/?g=" + gamecode.valueAsNumber;
+        window.location.href = "https://battleship-53140.web.app/?g=" + gamecode.valueAsNumber;
 });
