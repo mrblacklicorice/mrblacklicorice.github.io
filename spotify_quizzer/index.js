110ade33fa84237b2b71f70e6a4e2afdf6a15b92
Girish Dodda <girishbdodda@gmail.com>
2023-05-31 14:46:21 -0500

    thing

--- spotify_quizzer/index.js
+++ spotify_quizzer/index.js
@@ -167,7 +167,7 @@ function getUserData() {
 }
 
 function getUserPlaylistData() {
-    fetch('https://api.spotify.com/v1/me', {
+    fetch('https://api.spotify.com/v1/me/playlists', {
         headers: {
             Authorization: 'Bearer ' + access_token,
         },
