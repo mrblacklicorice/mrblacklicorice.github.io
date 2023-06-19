e1ae8f9a57639f54f0099786a5a79dd642de6458
Girish Dodda <61810026+mrblacklicorice@users.noreply.github.com>
2023-06-18 23:27:53 -0500

    thing

---
+++ Test/learn.js
@@ -0,0 +1,46 @@
+var x;
+let y;
+
+x = 1;
+y = 2;
+
+// JS Primitive Types
+// String "", ''
+// Number 1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+// Boolean true, false
+// undefined
+// null
+
+x = y;
+
+x == 2
+y == 2
+
+var z = 3;
+
+var list = [1, 2, 3, 4, 88, 6, 7, 8, 9, 0];
+
+list.push(1);
+
+list[list.length - 1];
+list[0];
+
+var cancer = 1;
+
+cancaer = cancer + 1;
+
+cancer += 1;
+cancer++;
+
+
+// var index = 0;
+
+// while (index < list.length) {
+//     console.log(list[index]);
+//     index++;
+// }
+
+for (let index = 0; index < list.length; index++) {
+    console.log(list[index]);
+}
+
