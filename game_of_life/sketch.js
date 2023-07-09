15b1564cb2c07c512784a4beac4bdb97014330b4
Girish Dodda <girishbdodda@gmail.com>
2023-07-08 23:30:06 -0500

    thing

--- game_of_life/sketch.js
+++ game_of_life/sketch.js
@@ -5,7 +5,7 @@ var cols = 50;
 var canvas;
 
 var idX, idY;
-var dim = 2500;
+var dim = 1000;
 var offsetX = dim / 2;
 var offsetY = dim / 2;
 
@@ -16,9 +16,13 @@ let pmouseY = null;
 var buttons = [
 	{ name: "Start", x1: 0, y1: 0, x2: 0, y2: 0, function: "startBtn", hover: false },
 	{ name: "Import", x1: 0, y1: 0, x2: 0, y2: 0, function: "importBtn", hover: false },
-	{ name: "Export", x1: 0, y1: 0, x2: 0, y2: 0, function: "exportBtn", hover: false }];
+	{ name: "Export", x1: 0, y1: 0, x2: 0, y2: 0, function: "exportBtn", hover: false },
+	{ name: "Patterns", x1: 0, y1: 0, x2: 0, y2: 0, function: "patternBtn", hover: false }
+];
 
 var board = new Array(dim);
+
+var details = document.getElementById("details");
 
 function setup() {
 	for (let i = 0; i < dim; i++) {
@@ -38,20 +42,37 @@ function setup() {
 function draw() {
 	clear();
 
+
+	// White Background
 	noStroke();
 	fill(255);
 	rect(0, 0, canvas.width, canvas.height);
 
+
+	// Draw all filled squares
 	for (let i = 0; i < rows; i++) {
 		for (let j = 0; j < cols; j++) {
 			// console.log(board[i][j]);
-			if (board[i + offsetX][j + offsetY]) {
+			if (board[(i + offsetX) % dim][(j + offsetY) % dim]) {
 				fill(0);
 				rect(i * pixel, j * pixel, pixel, pixel);
 			}
 		}
 	}
 
+	// Draw grid lines
+	stroke(125);
+	strokeWeight(pixel / 25);
+	for (let i = 0; i < rows; i++) {
+		line(i * pixel, 0, i * pixel, canvas.height);
+	}
+
+	for (let i = 0; i < cols; i++) {
+		line(0, i * pixel, canvas.width, i * pixel);
+	}
+
+	// Draw buttons
+	noStroke();
 	for (let i = 0; i < buttons.length; i++) {
 		if (buttons[i].x1 < mouseX && mouseX < buttons[i].x2 && buttons[i].y1 < mouseY && mouseY < buttons[i].y2 && !isMouseDragged) {
 			fill(200, 200, 200);
@@ -71,6 +92,7 @@ function draw() {
 		rectMode(CORNER);
 	}
 
+	// Draw cursor
 	if (!isMouseDragged && buttons.every((button) => !button.hover) && 0 <= idX && idX < rows && 0 <= idY && idY < cols) {
 		// stroke(pixel / 5);
 		fill(0, 0, 0, 100);
@@ -97,7 +119,10 @@ document.getElementById("file_upload").addEventListener("change", function () {
 	if (this.value) {
 		let reader = new FileReader();
 		reader.readAsText(this.files[0]);
+		console.log(reader);
 		reader.onload = function () {
+
+			console.log("loaded")
 			var tempBoard = JSON.parse(reader.result);
 
 			if (tempBoard.length == dim && tempBoard.every((row) => row.length == dim)) {
@@ -120,6 +145,8 @@ document.getElementById("file_upload").addEventListener("change", function () {
 		};
 	}
 
+	document.getElementById('file_upload').value = "";
+
 	loop();
 });
 
@@ -135,7 +162,24 @@ function exportBtn() {
 	loop();
 }
 
+function patternBtn() {
+	console.log("menu");
+	noLoop();
+	if (details.open) {
+		details.close();
+		loop();
+	}
+	else {
+		details.showModal();
+	}
+}
+
 function windowResized() {
+	if (details.open) {
+		details.close();
+		loop();
+	}
+
 	rows = Math.floor((windowWidth - padding) / pixel);
 	cols = Math.floor((windowHeight - padding) / pixel);
 
@@ -169,60 +213,77 @@ function mouseDragged() {
 	var changeY = Math.trunc((mouseY - pmouseY) / pixel);
 
 
-	if (changeX != 0 && offsetX - changeX >= 0 && offsetX - changeX <= dim - rows) {
+	if (changeX > 0) {
 		pmouseX = mouseX;
-		offsetX -= changeX;
-		console.log(changeX);
+		offsetX = offsetX - changeX;
+		if (offsetX < 0) {
+			offsetX = dim + offsetX;
+		}
+	} else if (changeX < 0) {
+		pmouseX = mouseX;
+		offsetX = (offsetX - changeX) % dim;
 	}
 
-	if (changeY != 0 && offsetY - changeY >= 0 && offsetY - changeY <= dim - cols) {
+	console.log(changeX + ", " + changeY);
+
+	if (changeY > 0) {
 		pmouseY = mouseY;
-		offsetY -= changeY
-		console.log(changeY);
+		offsetY = offsetY - changeY;
+		if (offsetY < 0) {
+			offsetY = dim + offsetY;
+		}
+	} else if (changeY < 0) {
+		pmouseY = mouseY;
+		offsetY = (offsetY - changeY) % dim;
 	}
 
 	// console.log(offsetX + ", " + offsetY);
 }
 
 function mouseReleased() {
-	for (let i = 0; i < buttons.length; i++) {
-		if (buttons[i].hover) {
-			window[buttons[i].function]();
+	if (isLooping()) {
+		for (let i = 0; i < buttons.length; i++) {
+			if (buttons[i].hover) {
+				window[buttons[i].function]();
+			}
 		}
-	}
 
-	if (!isMouseDragged && buttons.every((button) => !button.hover)) {
-		board[offsetX + idX][offsetY + idY] = !board[offsetX + idX][offsetY + idY];
-	}
+		if (!isMouseDragged && buttons.every((button) => !button.hover)) {
+			board[(offsetX + idX) % dim][(offsetY + idY) % dim] = !board[(offsetX + idX) % dim][(offsetY + idY) % dim];
+		}
 
-	pmouseX = null;
-	pmouseY = null;
-	isMouseDragged = false;
+		pmouseX = null;
+		pmouseY = null;
+		isMouseDragged = false;
+	}
 }
 
 function mouseWheel(event) {
-	var tempX = Math.round(mouseX / pixel);
-	var tempY = Math.round(mouseY / pixel);
-	var tempP = pixel;
+	if (isLooping()) {
 
-	if (event.delta < 0 && pixel < 64) {
-		// Zoom in
-		pixel *= 1.5;
-		pixel = Math.round(pixel);
-		offsetX += Math.floor((1 - (tempP / pixel)) * tempX);
-		offsetY += Math.floor((1 - (tempP / pixel)) * tempY);
+		var tempX = Math.round(mouseX / pixel);
+		var tempY = Math.round(mouseY / pixel);
+		var tempP = pixel;
 
-	} else if (event.delta > 0 && pixel > 8) {
-		// Zoom out
-		pixel /= 1.5;
-		pixel = Math.round(pixel);
-		offsetX -= Math.floor(((tempP / pixel) - 1) * tempX);
-		offsetY -= Math.floor(((tempP / pixel) - 1) * tempY);
+		if (event.delta < 0 && pixel < 64) {
+			// Zoom in
+			pixel *= 1.5;
+			pixel = Math.round(pixel);
+			offsetX += Math.floor((1 - (tempP / pixel)) * tempX);
+			offsetY += Math.floor((1 - (tempP / pixel)) * tempY);
+
+		} else if (event.delta > 0 && pixel > 8) {
+			// Zoom out
+			pixel /= 1.5;
+			pixel = Math.round(pixel);
+			offsetX -= Math.floor(((tempP / pixel) - 1) * tempX);
+			offsetY -= Math.floor(((tempP / pixel) - 1) * tempY);
+		}
+
+		mouseMoved();
+		windowResized();
+
+		// Disable page scroll
+		return false;
 	}
-
-	mouseMoved();
-	windowResized();
-
-	// Disable page scroll
-	return false;
 }
