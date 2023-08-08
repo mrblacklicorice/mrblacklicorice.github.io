71df6434db6153981f361d5a2c23816bbdf16e6b
Girish Dodda <girishbdodda@gmail.com>
2023-08-08 00:20:47 -0500

    leetcode

--- Test/leetcode.js
+++ Test/leetcode.js
@@ -4777,49 +4777,37 @@ var searchRS = function (nums, target) {
 var search = function (nums, target) {
   var i = 0;
 
-  var start = 1;
+  var start = 0;
   var end = nums.length - 1;
+  var mid;
 
-  var once = false;
+  while (start <= end) {
+    mid = Math.floor((start + end) / 2);
 
-  for (let j = 0; j < 2; j++) {
-    while (start <= end) {
-      var mid = Math.floor((start + end) / 2);
-      // console.log(mid, start, end);
+    if (nums[mid] == target) return true;
 
-      if (nums[mid - 1] > nums[mid]) {
-        j++;
-        i = mid;
-        break;
-      } else if (nums[mid] < nums[0]) {
-        end = mid - 1;
-      } else if (nums[mid] == nums[0]) {
-        if (once) end = mid - 1;
-        else start = mid + 1;
-      } else {
-        end = mid - 1;
-      }
+    if ((start == end) || (nums[mid] != nums[start] && nums[mid] != nums[end])) {
+      if (nums[mid - 1] > nums[mid]) end = -10;
+      else if (nums[mid] < nums[0]) end = mid - 1;
+      else start = mid + 1;
+    } else {
+      if (nums[mid] == nums[start]) start++;
+      if (nums[mid] == nums[end]) end--;
     }
-    once = false;
   }
 
-  console.log(i);
+  if (end == -10) i = mid;
   start = 0;
   end = nums.length - 1;
   while (start <= end) {
-    var mid = Math.floor((start + end) / 2);
+    mid = Math.floor((start + end) / 2);
 
-    if (nums[(mid + i) % nums.length] === target) {
-      // console.log((mid + i) % nums.length);
-      return true;
-    } else if (nums[(mid + i) % nums.length] < target) {
-      start = mid + 1;
-    } else {
-      end = mid - 1;
-    }
+    if (nums[(mid + i) % nums.length] === target) return true;
+    else if (nums[(mid + i) % nums.length] < target) start = mid + 1;
+    else end = mid - 1;
   }
 
   return false;
 };
 
-// console.log(search([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1], 2));
+console.log(search([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1], 2));
